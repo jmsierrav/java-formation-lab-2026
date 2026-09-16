550ff9441244accab17d251eef49762359406da1
Jesus Sierra <jmsierrav@indracompany.com>
2026-09-16 14:45:41 -0500

    fix: optimización de imports

--- sessions/week-04-patrones-strategy-factory/src/main/java/com/indra/logistics/base/PaymentController.java
+++ sessions/week-04-patrones-strategy-factory/src/main/java/com/indra/logistics/base/PaymentController.java
@@ -1,8 +1,5 @@
 package com.indra.logistics.base;
 
-import java.math.BigDecimal;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 
 @RestController
